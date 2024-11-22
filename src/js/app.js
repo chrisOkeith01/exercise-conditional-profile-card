5f47d60e8d8a009ef07f82ff9e2def334e63a17c
chrisOkeith01 <christopherkeith70@gmail.com>
2024-11-21 19:42:13 -0500

    Update app.js

--- src/js/app.js
+++ src/js/app.js
@@ -33,10 +33,15 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>Lucy Boilett</h1>
-          <h2>Web Developer</h2>
-          <h3>Miami, USA</h3>
-          <ul class="position-right">
+          <h1>${variables.name ? variables.name : "first"}
+         ${
+           
+              variables.lastName ? variables.lastName :"last"
+         } 
+          </h1>
+          <h2>${variables.role ? variables.role : "job title"}</h2>
+          <h3>${variables.city ? variables.city : "city" }</h3>
+          <ul class="${variables.socialMediaPosition}">
             <li><a href="https://twitter.com/4geeksacademy"><i class="fab fa-twitter"></i></a></li>
             <li><a href="https://github.com/4geeksacademy"><i class="fab fa-github"></i></a></li>
             <li><a href="https://linkedin.com/school/4geeksacademy"><i class="fab fa-linkedin"></i></a></li>
@@ -58,7 +63,7 @@ window.onload = function() {
     // this is the url for the profile avatar
     avatarURL: "https://randomuser.me/api/portraits/women/42.jpg",
     // social media bar position (left or right)
-    socialMediaPosition: "position-left",
+    socialMediaPosition: "position-left or right",
     // social media usernames
     twitter: null,
     github: null,
